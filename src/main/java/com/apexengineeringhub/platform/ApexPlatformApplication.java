8a06c988756eeb4b863c0bfb5e9d7e0d4b435159
AnjaniKrPathak <eranjanipathak.it@gmail.com>
2026-07-01 22:47:28 +0530

    feat(project): initialize Spring Boot project

---
+++ src/main/java/com/apexengineeringhub/platform/ApexPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.apexengineeringhub.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apexengineeringhub/platform/ApexPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexengineeringhub.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
